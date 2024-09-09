b5ff3a69ef9b7d6db4942b11e76779c446fa5c1a
ChinmayKaitade <chinmaykaitade123@gmail.com>
2024-09-09 20:17:47 +0530

    Loader animation added

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ gsap.from(".line h1", {
   duration: 0.6,
   delay: 0.5,
 });
+
+let h5timer = document.querySelector("#line1-part1 h5");
+let grow = 0;
+
+setInterval(() => {
+  if (grow < 100) {
+    h5timer.innerHTML = grow++;
+  } else {
+    h5timer.innerHTML = grow;
+  }
+}, 35);
