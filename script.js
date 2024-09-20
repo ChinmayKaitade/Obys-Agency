ee1f1fde25c3675640d5731e88e0e725855e3f68
ChinmayKaitade <chinmaykaitade123@gmail.com>
2024-09-20 15:29:24 +0530

    Page2 show video and cursor icon added

--- script.js
+++ script.js
@@ -52,8 +52,8 @@ function loadingAnimation() {
     opacity: 0,
   });
 
-  tl.from(".hero h1", {
-    y: 120,
+  tl.from("#hero1 h1, #hero2 h1, #hero3 h2, #hero4 h1", {
+    y: 140,
     stagger: 0.2,
   });
 }
